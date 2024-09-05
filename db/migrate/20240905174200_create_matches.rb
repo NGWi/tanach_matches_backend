823e463f1c9718f3425b33157f8007d5ecb03f1e
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2024-09-05 14:04:10 -0400

    Corrected matches migration and successfully initialized db

--- db/migrate/20240905174200_create_matches.rb
+++ db/migrate/20240905174200_create_matches.rb
@@ -2,7 +2,7 @@ class CreateMatches < ActiveRecord::Migration[7.1]
   def change
     create_table :matches do |t|
       t.references :word, null: false, foreign_key: true
-      t.references :matched_word, null: false, foreign_key: true
+      t.references :matched_word, null: false, foreign_key: { to_table: :words }
 
       t.timestamps
     end
